67b8e3609792095d7cafef4a05edcf77a91f1afc
Kevin M <kevin.mueller1@outlook.com>
2026-02-13 20:16:45 +0100

    fix runnables for test classes without attribute

--- languages/csharp/runnables.scm
+++ languages/csharp/runnables.scm
@@ -51,17 +51,29 @@
   (#set! tag csharp-test-class)
 )
 
-; ==============================
-; Test class without namespace
+; ================================================
+; Test class by name ending with "Test" (with namespace)
+; ================================================
+(
+  (namespace_declaration
+    name: (_) @csharp_namespace
+    body: (declaration_list
+      (class_declaration
+        name: (identifier) @run @csharp_class_name
+        (#match? @csharp_class_name "Test$"))))
+  (#set! tag csharp-test-class)
+)
+
+
+; =================================================
+; Test class by name ending with "Test" (without namespace)
 ; (anchored to top-level only)
-; ==============================
+; =================================================
 (
   (compilation_unit
     (class_declaration
-      (attribute_list
-        (attribute
-          name: (_) @class_attribute
-          (#match? @class_attribute "^(TestFixture|TestClass)$")))
-      name: (identifier) @run @csharp_class_name))
+      name: (identifier) @run @csharp_class_name
+      (#match? @csharp_class_name "Test$")))
   (#set! tag csharp-test-class)
 )
+
